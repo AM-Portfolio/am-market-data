2eb31dc17df62ae6040e7ea28043a92cd4270a00
Munish Kumar <40138449+ssd2658@users.noreply.github.com>
2025-03-25 07:43:06 +0530

    api for nse-indices and re cook data is getting published

--- market-data-api/src/main/java/com/am/marketdata/api/controller/MarketIndexController.java
+++ market-data-api/src/main/java/com/am/marketdata/api/controller/MarketIndexController.java
@@ -2,7 +2,7 @@ package com.am.marketdata.api.controller;
 
 import com.am.common.investment.model.stockindice.StockIndicesMarketData;
 import com.am.common.investment.service.StockIndicesMarketDataService;
-
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +15,9 @@ public class MarketIndexController {
     private StockIndicesMarketDataService marketIndexService;
 
     @GetMapping("/{indexSymbol}")
-    public ResponseEntity<StockIndicesMarketData> getLatestIndexData(@PathVariable String indexSymbol) {
+    public ResponseEntity<StockIndicesMarketData> getLatestIndexData(
+            @Parameter(description = "NSE Index symbol", required = true)
+            @PathVariable("indexSymbol") String indexSymbol) {
         return ResponseEntity.ok(marketIndexService.findByIndexSymbol(indexSymbol));
     }
 }
