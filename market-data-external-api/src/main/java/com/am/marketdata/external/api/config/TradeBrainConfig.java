06262cb9c735ea20658d880548469a547c1c785f
Munish Kumar <40138449+ssd2658@users.noreply.github.com>
2025-03-28 00:01:22 +0530

    update multiple endpointa

--- market-data-external-api/src/main/java/com/am/marketdata/external/api/config/TradeBrainConfig.java
+++ market-data-external-api/src/main/java/com/am/marketdata/external/api/config/TradeBrainConfig.java
@@ -16,7 +16,74 @@ public class TradeBrainConfig {
 
     @Data
     public static class Api {
-        private String indexAll;
+        private Company company;
+        private Indices indices;
+    }
+
+    @Data
+    public static class Company {
+        private Profile profile;
+        private Financial financial;
+        private Documents documents;
+        private Reports reports;
+        private Stock stock;
+        private Analytics analytics;
+    }
+
+    @Data
+    public static class Analytics {
+        private String analytics;
+    }
+
+    @Data
+    public static class Profile {
+        private String shortDetails;
+        private String companyProfile;
+        private String companyInsights;
+        private String keymetrics;
+        private String researchReport;
+        private String corporateActions;
+        private String topShareholders;
+        private String companyScore;
+        private String boardOfDirectors;
+    }
+
+    @Data
+    public static class Financial {
+        private String dividends;
+        private String halfYearlyStatement;
+        private String quarterlyResultsStandalone;
+        private String profitAndLossStandalone;
+        private String balanceSheetStandalone;
+        private String cashFlowStandalone;
+    }
+
+    @Data
+    public static class Documents {
+        private String creditRating;
+        private String industryPeers;
+    }
+
+    @Data
+    public static class Reports {
+        private String directorReport;
+        private String chairmanReport;
+        private String auditorReport;
+        private String annualReports;
+    }
+
+    @Data
+    public static class Stock {
+        private String details;
+        private String historical;
+        private String technical;
+        private String shareholding;
+        private String heatmap;
+    }
+
+    @Data
+    public static class Indices {
+        private String all;
     }
 
     @Data
