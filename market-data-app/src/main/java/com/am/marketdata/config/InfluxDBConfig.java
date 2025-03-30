5897711c52785f630f5100d7653dbee020dc0b14
Munish Kumar <40138449+ssd2658@users.noreply.github.com>
2025-03-31 03:59:59 +0530

    some how it is working. api started responding.

---
+++ market-data-app/src/main/java/com/am/marketdata/config/InfluxDBConfig.java
@@ -0,0 +1,30 @@
+package com.am.marketdata.config;
+
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.InfluxDBClientFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class InfluxDBConfig {
+
+    @Value("${spring.influx.url}")
+    private String url;
+
+    @Value("${spring.influx.token}")
+    private String token;
+
+    @Value("${spring.influx.org}")
+    private String org;
+
+    @Value("${spring.influx.bucket}")
+    private String bucket;
+
+    @Bean
+    @Primary
+    public InfluxDBClient influxDBClient() {
+        return InfluxDBClientFactory.create(url, token.toCharArray(), org, bucket);
+    }
+}
